b4b3124b6f6e6751a0d977c71b0eec47aa975010
Bryan Foster <bfoster@cts-software.com>
2014-06-16 14:31:17 -0500

    Changes made by Bryan Foster (bfoster@cts-software.com).

--- ParaScope/scripts/app/constants.js
+++ ParaScope/scripts/app/constants.js
@@ -5,16 +5,16 @@ function () {
         //why does this not work... serviceUrl: me.baseUrl + "services/TabletDispatcher.svc/",
         baseUrl: "",
         serviceUrl: "",
-        liveBaseUrl: "https://tripmasterenterprise.com/",
-        liveServiceUrl: "https://tripmasterenterprise.com/" + "services/TabletDispatcher.svc/",
-        qaBaseUrl: "https://qa.tripmasterenterprise.com/",
-        qaServiceUrl: "https://qa.tripmasterenterprise.com/" + "services/TabletDispatcher.svc/",
+        liveBaseUrl: "http://tripmasterenterprise.com/",
+        liveServiceUrl: "http://tripmasterenterprise.com/" + "services/TabletDispatcher.svc/",
+        qaBaseUrl: "http://qa.tripmasterenterprise.com/",
+        qaServiceUrl: "http://qa.tripmasterenterprise.com/" + "services/TabletDispatcher.svc/",
         devBaseUrl: "http://bryanhfoster.dyndns.org/",
         devServiceUrl: "http://bryanhfoster.dyndns.org/" + "services/TabletDispatcher.svc/",
         publishKey: "pub-c-08992f84-406c-490b-b7f6-0a17e9f577e8",
         subscribeKey: "sub-c-7326670e-eedc-11e2-b383-02ee2ddab7fe",
         templateDirectory: "scripts/templates/",
-        currentSoftwareVersion: 1.7
+        currentSoftwareVersion: 1.6
     };  
         
     return me;
